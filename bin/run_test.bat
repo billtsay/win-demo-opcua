ecf7e2f53c495de03362f3bba2bcc007efb760d1
bill tsay <billtsay@yahoo.com>
2016-05-02 21:53:59 -0700

    update set pythonpath script.

--- bin/run_test.bat
+++ bin/run_test.bat
@@ -1,3 +1,5 @@
-set PYTHONPATH=%PYTHONPATH%;C:\..\win-demo-opcua\Python27;C:\..\win-demo-opcua\bin
-# C:\PYTHON27;C:\PYTHON27DLLs;C:\PYTHON27\LIB;C:\PYTHON27\LIB\LIB-TK
-set PATH=%PATH%;C:\..\win-demo-opcua\Python27
+echo off
+set HOME=C:\Users\"jiannliang tsay"\git
+set PYTHONPATH=%PYTHONPATH%;%HOME%\win-demo-opcua\PYTHON27;%HOME%\win-demo-opcua\PYTHON27DLLs;%HOME%\win-demo-opcua\PYTHON27\LIB;%HOME%\win-demo-opcua\PYTHON27\LIB\LIB-TK
+set PYTHONPATH=%PYTHONPATH%;%HOME%\win-demo-opcua\Python27;%HOME%\win-demo-opcua\bin
+set PATH=%PATH%;%HOME%\win-demo-opcua\Python27
