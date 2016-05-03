5959a41daaa66a5dfc2724e23bbec81488ccd85a
bill tsay <billtsay@yahoo.com>
2016-05-02 21:47:39 -0700

    add set pythonpath.

---
+++ bin/run_test.bat
@@ -0,0 +1,3 @@
+set PYTHONPATH=%PYTHONPATH%;C:\..\win-demo-opcua\Python27;C:\..\win-demo-opcua\bin
+# C:\PYTHON27;C:\PYTHON27DLLs;C:\PYTHON27\LIB;C:\PYTHON27\LIB\LIB-TK
+set PATH=%PATH%;C:\..\win-demo-opcua\Python27
